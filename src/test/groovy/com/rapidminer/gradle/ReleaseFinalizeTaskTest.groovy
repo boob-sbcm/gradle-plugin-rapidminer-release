8b564233c05b0475ef481889aa6026b6f93034f2
Nils Wöhler <ncwoehler@gmail.com>
2014-08-03 22:17:06 +0200

    First tests for ReleaseFinalize task

--- src/test/groovy/com/rapidminer/gradle/ReleaseFinalizeTaskTest.groovy
+++ src/test/groovy/com/rapidminer/gradle/ReleaseFinalizeTaskTest.groovy
@@ -30,5 +30,105 @@ import org.gradle.api.GradleException
  */
 class ReleaseFinalizeTaskTest extends AbstractReleaseTaskSpecification {
 
+	def 'check standalone run of finalize will fail'() {
+		when:
+		ExecutionResult result = runTasksWithFailure(ReleasePlugin.FINALIZE_TASK_NAME)
+
+		then:
+		result.failure.cause.cause.message == 'Unknown release branch. Release finalize cannot be run as standalone task!'
+	}
+	
+	def 'check finalize without remote branch and release branch develop'() {
+		given:
+		buildFile << '''
+			releaseFinalize {
+				releaseBranch = 'develop'
+			}
+		'''
+		
+		when:
+		ExecutionResult result = runTasks(ReleasePlugin.FINALIZE_TASK_NAME)
+
+		then:
+		result.standardOutput.contains 'Release was made from develop. No need to merge changes back.'
+		propertiesFile.text.contains 'version=1.0.1-SNAPSHOT'
+		scmProvider.currentBranch == 'develop'
+	}
+	
+	def 'check finalize without remote branch and release branch release'() {
+		given:
+		repo.checkout(branch: 'release', createBranch: true)
+		scmProvider.switchToBranch('master')
+		
+		buildFile << '''
+			releaseFinalize {
+				releaseBranch = 'release'
+			}
+		'''
+		
+		when:
+		ExecutionResult result = runTasks(ReleasePlugin.FINALIZE_TASK_NAME)
+
+		then:
+		result.standardOutput.contains 'Switched to branch: release'
+		result.standardOutput.contains 'Release branch wasn\'t \'develop\' branch. Merging release branch to develop.'
+		result.standardOutput.contains 'Merged \'release\' into current branch.'
+		result.standardOutput.contains 'Deleting release branches [release].'
+		propertiesFile.text.contains 'version=1.0.1-SNAPSHOT'
+		scmProvider.currentBranch == 'develop'
+	}
+	
+	def 'check finalize without remote branch and release branch release without merge to develop'() {
+		given:
+		repo.checkout(branch: 'release', createBranch: true)
+		scmProvider.switchToBranch('master')
+		
+		buildFile << '''
+			releaseFinalize {
+				releaseBranch = 'release'
+			}
+
+			release {
+				mergeToDevelop = false
+			}
+		'''
+		
+		when:
+		ExecutionResult result = runTasks(ReleasePlugin.FINALIZE_TASK_NAME)
+
+		then:
+		result.standardOutput.contains 'Switched to branch: release'
+		result.standardOutput.contains 'Release branch was not develop. Check if changes should be merged back to develop.'
+		result.standardOutput.contains 'Merging changes from \'release\' back to develop is disabled.'
+		propertiesFile.text.contains 'version=1.0.1-SNAPSHOT'
+		scmProvider.currentBranch == 'release'
+	}
+
+	def 'check finalize without remote branch and do not delete release branch'() {
+		given:
+		repo.checkout(branch: 'release', createBranch: true)
+		scmProvider.switchToBranch('master')
+		
+		buildFile << '''
+			releaseFinalize {
+				releaseBranch = 'release'
+			}
+
+			release {
+				deleteReleaseBranch = false
+			}
+		'''
+		
+		when:
+		ExecutionResult result = runTasks(ReleasePlugin.FINALIZE_TASK_NAME)
+
+		then:
+		result.standardOutput.contains 'Switched to branch: release'
+		result.standardOutput.contains 'Release branch wasn\'t \'develop\' branch. Merging release branch to develop.'
+		result.standardOutput.contains 'Merging changes from \'release\' branch to \'develop\' branch.'
+		result.standardOutput.contains 'Skipping deletion of release branch as it is disabled.'
+		propertiesFile.text.contains 'version=1.0.1-SNAPSHOT'
+		scmProvider.currentBranch == 'develop'
+	}
 
 }
